a5d58a8fad2ceeb882e7ed28abb04b2ba9f1a6bb
RihannaOnTheBrain <361201165@qq.com>
2016-10-30 06:42:13 -0700

    Initial Commit

---
+++ devslopes-social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-social
+//
+//  Created by lemonade on 16/10/30.
+//  Copyright © 2016年 lemonade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
