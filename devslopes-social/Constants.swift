a905749adc2774c48ff76c8f12667af8dacb8d43
RihannaOnTheBrain <361201165@qq.com>
2016-11-01 07:33:09 -0700

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by lemonade on 16/11/1.
+//  Copyright © 2016年 lemonade. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
